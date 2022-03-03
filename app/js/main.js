6240f6c65ef39dc8257fb88ba51ac05d1d28e02c
cakmaksevgi <sevgickmk.7@gmail.com>
2022-03-04 02:34:41 +0300

    implemented scale part

--- app/js/main.js
+++ app/js/main.js
@@ -9,34 +9,68 @@ const AppView = () => {
             </fieldset>
         </form>
 
+        <div class="scale-container">
+            <input id="scaleVal" class="scale-input" onchange=onChangeScaleValue(this.value) type="number" placeholder="Please enter your photo(s) scale.. " />
+            <button id="scaleBtn" class="scale-btn" onclick="scalePhoto()" disabled>Scale Photo</button>
+        </div>
+
         <canvas id="editorCanvas"></canvas>`;
 
     // grab DOM elements inside index.html
-    const fileSelector = document.getElementById( "fileSelector" );
-    const editorCanvas = document.getElementById( "editorCanvas" );
+    const fileSelector = document.getElementById("fileSelector");
+    const editorCanvas = document.getElementById("editorCanvas");
 
-    fileSelector.onchange = function( e ) {
+    window.addEventListener('DOMContentLoaded', (event) => {
+        window.scaleButton = document.getElementById('scaleBtn');
+    });
+
+    window.fileUploaded = false;
+    window.scaleValue = null;
+    window.imgObj = null;
+    window.scalePhoto = function () {
+        if (!window.fileUploaded) {
+            alert("Please select an image file..");
+            return;
+        }
+
+        const ctx = editorCanvas.getContext('2d');
+        const value = parseInt(window.scaleValue) / 100;
+        const newWidth = window.imgObj.naturalWidth * value;
+        const newHeight = window.imgObj.naturalHeight * value;
+        editorCanvas.width = newWidth;
+        editorCanvas.height = newHeight;
+        ctx.drawImage(window.imgObj, 0, 0, newWidth, newHeight);
+    }
+
+    window.onChangeScaleValue = function (val) {
+        window.scaleValue = val;
+        scaleButton.disabled = !val;
+    }
+
+    fileSelector.onchange = function (e) {
         // get all selected Files
         const files = e.target.files;
         let file;
-        for ( let i = 0; i < files.length; ++i ) {
-            file = files[ i ];
+        for (let i = 0; i < files.length; ++i) {
+            file = files[i];
             // check if file is valid Image (just a MIME check)
-            switch ( file.type ) {
+            switch (file.type) {
                 case "image/jpeg":
                 case "image/png":
                 case "image/gif":
+                    window.fileUploaded = true;
                     // read Image contents from file
                     const reader = new FileReader();
-                    reader.onload = function( e ) {
+                    reader.onload = function (e) {
                         // create HTMLImageElement holding image data
                         const img = new Image();
                         img.src = reader.result;
 
-                        img.onload = function() {
+                        img.onload = function () {
                             // grab some data from the image
                             const width = img.naturalWidth;
                             const height = img.naturalHeight;
+                            window.imgObj = img;
 
                             editorCanvas.width = 500;
                             editorCanvas.height = 500 * height / width;
@@ -45,10 +79,11 @@ const AppView = () => {
                         }
                         // do your magic here...
                     };
-                    reader.readAsDataURL( file );
+                    reader.readAsDataURL(file);
                     // process just one file.
                     return;
-
+                default:
+                    alert("Some of your file(s) cannot process for now..");
             }
         }
     };
