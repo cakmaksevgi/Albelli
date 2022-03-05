c9a08c6124c2969cc36f9b87ed383341236aff1d
cakmaksevgi <sevgickmk.7@gmail.com>
2022-03-06 02:12:07 +0300

    implemented import process for already saved images

--- app/js/main.js
+++ app/js/main.js
@@ -4,13 +4,23 @@ const AppView = () => {
     document.body.innerHTML = `<h1>Simple Example</h1>
         <form action="javascript:savePhoto();">
             <fieldset>
-                <label for="fileSelector">Select an Image file</label>
-                <input type="file" id="fileSelector" />
+                <div class="d-flex">
+                    <div>
+                        <label for="fileSelector">Select an Image file</label>
+                        <input type="file" id="fileSelector" />
+                    </div>
+                    <div class="mx-2">|</div>
+                    <div>
+                        <label>Import an Image file</label>
+                        <select id="importList" onchange="onChangeImage(this)">
+                        </select>
+                    </div>
+                </div>
 
                 <div class="scale-container">
-                    <input id="scaleVal" class="scale-input" onchange=onChangeScaleValue(this.value) type="number"
+                    <input id="scaleVal" class="scale-input" onchange="onChangeScaleValue(this.value)" type="number"
                          placeholder="Please enter your photo(s) scale.. " max="100"/>
-                     <button id="scaleBtn" class="scale-btn" onclick="scalePhoto()" disabled>Scale Photo</button>
+                     <button id="scaleBtn" class="scale-btn" onclick="scalePhoto(event)" disabled>Scale Photo</button>
                 </div>
             </fieldset>
             <input class="m-1" id="submit" type="submit" value="Submit">
@@ -28,12 +38,14 @@ const AppView = () => {
 
     window.addEventListener('DOMContentLoaded', (event) => {
         window.scaleButton = document.getElementById('scaleBtn');
+        window.prepareSelectBox();
     });
 
     window.fileUploaded = false;
     window.scaleValue = null;
     window.imgObj = null;
-    window.scalePhoto = () => {
+    window.scalePhoto = (e) => {
+        e.preventDefault();
         if (!window.fileUploaded) {
             alert("Please select an image file..");
             return;
@@ -47,13 +59,17 @@ const AppView = () => {
             return;
         }
 
-        const scalePercentage = scale / 100;
-        const ctx = editorCanvas.getContext('2d');
+        const scalePercentage = scale / 100; // percentage of scale
         const newWidth = window.imgObj.naturalWidth * scalePercentage;
         const newHeight = window.imgObj.naturalHeight * scalePercentage;
+        window.renderImage(window.imgObj, newWidth, newHeight);
+    }
+    window.renderImage = (img, newWidth, newHeight) => {
+        const ctx = editorCanvas.getContext('2d');
         editorCanvas.width = newWidth;
         editorCanvas.height = newHeight;
-        ctx.drawImage(window.imgObj, 0, 0, newWidth, newHeight);
+        ctx.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight,     // source rectangle
+            0, 0, editorCanvas.width, editorCanvas.height); // destination rectangle
     }
     window.moveTo = (right) => {
         const elem = document.getElementById("canvas-container");
@@ -64,11 +80,73 @@ const AppView = () => {
         window.scaleValue = val;
         scaleButton.disabled = !val;
     }
-    window.savePhoto =  () => {
+    window.savePhoto = () => {
         if (!window.fileUploaded) {
             alert("Please select an image file..");
             return;
         }
+
+        let storedImages = getStoredImages();
+        const data = {
+            "canvas": {
+                "width": editorCanvas.width,
+                "height": editorCanvas.height,
+                "photo": {
+                    "id": window.imgObj.name,
+                    "width": window.imgObj.naturalWidth,
+                    "height": window.imgObj.naturalHeight,
+                    "x": window.scrollX + document.querySelector('#editorCanvas').getBoundingClientRect().left, // X,
+                    "y": window.scrollY + document.querySelector('#editorCanvas').getBoundingClientRect().top, // Y
+                    "src": window.imgObj.src
+                }
+            }
+        };
+        storedImages[window.imgObj.name] = data;
+        window.localStorage.setItem("imgData", JSON.stringify(storedImages));
+        window.prepareSelectBox();
+    }
+    window.prepareSelectBox = () => {
+        const selectElem = document.getElementById("importList");
+        selectElem.replaceChildren();
+        const images = getStoredImages();
+        window.createOption("Select an image..", selectElem, "default");
+        for (const val in images) {
+            window.createOption(val, selectElem);
+        }
+    }
+    window.createOption = (optionValue, selectContainer) => {
+        const option = document.createElement("option");
+        option.value = optionValue;
+        option.text = optionValue;
+        selectContainer.appendChild(option);
+    }
+    window.onChangeImage = (selectedImg) => {
+        const key = selectedImg.value;
+        if (key === "Select an image..") {
+            window.changeCanvasVisible(false);
+            return;
+        }
+        const images = getStoredImages();
+        const currentImg = images[key];
+        const image = new Image();
+        image.src = currentImg.canvas.photo.src;
+        image.onload = () => {
+            window.renderImage(image, currentImg.canvas.width, currentImg.canvas.height);
+            window.changeCanvasVisible();
+        }
+    }
+
+    window.getStoredImages = () => {
+        let storedImages = JSON.parse(window.localStorage.getItem("imgData"));
+        if (!storedImages) {
+            storedImages = {};
+        }
+        return storedImages;
+    }
+
+    window.changeCanvasVisible = (visible = true) => {
+        //visible canvas container..
+        document.getElementById("canvas-container").className = visible ? "d-flex" : "d-none";
     }
 
     fileSelector.onchange = (e) => {
@@ -95,14 +173,14 @@ const AppView = () => {
                             const width = img.naturalWidth;
                             const height = img.naturalHeight;
                             window.imgObj = img;
+                            window.imgObj.name = file.name;
 
                             editorCanvas.width = 500;
                             editorCanvas.height = 500 * height / width;
                             const ctx = editorCanvas.getContext('2d');
                             ctx.drawImage(img, 0, 0, width, height, 0, 0, editorCanvas.width, editorCanvas.height);
 
-                            //visible canvas container..
-                            document.getElementById("canvas-container").className = "d-flex";
+                            window.changeCanvasVisible();
                         }
                         // do your magic here...
                     };
